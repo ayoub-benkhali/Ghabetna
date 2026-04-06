5f0219c1133910bbb02f0d5493c930649780e0fd
Azer <azer.radhouene@fsb.ucar.tn>
2026-04-06 22:40:23 +0100

    fixed email activation url

--- backend/services/auth_service/app/config.py
+++ backend/services/auth_service/app/config.py
@@ -1,24 +1,15 @@
-from pathlib import Path
-from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-env_path = Path(__file__).parent.parent.parent.parent / '.env'
-load_dotenv(dotenv_path=env_path)
-
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=env_path,
-        extra="allow"
-    )
+    model_config = SettingsConfigDict(extra="allow")
 
-    AUTH_DATABASE_URL: str           
-    AUTH_DATABASE_URL_SYNC: str     
+    AUTH_DATABASE_URL: str
+    AUTH_DATABASE_URL_SYNC: str
     REDIS_URL: str = "redis://localhost:6379/0"
-    
     JWT_SECRET: str
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
@@ -29,9 +20,7 @@ class Settings(BaseSettings):
     MAIL_SERVER: str = "smtp.gmail.com"
     MAIL_PORT: int = 587
     FRONTEND_URL: str = "http://localhost:3000"
-    FOREST_SERVICE_URL:str="http://forest-service:8000"
+    FOREST_SERVICE_URL: str = "http://forest-service:8000"
 
-settings = Settings()  # type: ignore
-logger.info(f"the path variable exists: {env_path.exists()}")
-logger.info(f"AUTH_DATABASE_URL: {settings.AUTH_DATABASE_URL}")
+settings = Settings() # type: ignore
 logger.info(f"FRONTEND_URL: {settings.FRONTEND_URL}")
